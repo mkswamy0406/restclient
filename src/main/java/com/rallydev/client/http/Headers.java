1ac06306b34ce293614f5fcb7e6f17af4c1e9088
Adam Esterline and Steve Neely <pair+ae+sn@rallydev.com>
2012-09-14 08:57:59 -0600

    Adding TraceId header code to the client

--- src/main/java/com/rallydev/client/http/Headers.java
+++ src/main/java/com/rallydev/client/http/Headers.java
@@ -3,10 +3,13 @@ package com.rallydev.client.http;
 import org.apache.http.Header;
 import org.apache.http.message.BasicHeader;
 
+import java.util.UUID;
+
 import static com.rallydev.client.http.Resource.isValidForType;
 
 public abstract class Headers {
     public static final String ZUUL_AUTH_HEADER_NAME = "Authorization";
+    public static final String X_TRACE_ID = "X-Trace-Id";
 
     public static Header ZuulAuthentication(String authKeyRef) {
         if(isValidForType("key", authKeyRef)) {
@@ -15,5 +18,11 @@ public abstract class Headers {
         }
 
         throw new IllegalArgumentException(String.format("Authorization key ref is invalid: %s", authKeyRef));
+    }
+
+    public static Header TraceId(String traceId) {
+        String currentTraceId = traceId != null ? traceId : UUID.randomUUID().toString();
+
+        return new BasicHeader(X_TRACE_ID, currentTraceId);
     }
 }
--- src/test/java/com/rallydev/client/http/HeadersTest.java
+++ src/test/java/com/rallydev/client/http/HeadersTest.java
@@ -3,11 +3,13 @@ package com.rallydev.client.http;
 import org.apache.http.Header;
 import org.testng.annotations.Test;
 
+import static com.rallydev.client.http.Headers.TraceId;
 import static com.rallydev.client.http.Headers.ZUUL_AUTH_HEADER_NAME;
 import static com.rallydev.client.http.Headers.ZuulAuthentication;
 import static java.util.UUID.randomUUID;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.testng.Assert.fail;
 
 @Test
@@ -54,5 +56,20 @@ public class HeadersTest {
 
         assertThat(header.getName(), is(ZUUL_AUTH_HEADER_NAME));
         assertThat(header.getValue(), is(String.format("Zuul authentication_key=\"%s\"", keyRef)));
+    }
+
+    public static void shouldReturnTheCorrectHeaderWithAGoodTraceId() {
+        String traceId = "fooobar";
+        Header header = TraceId(traceId);
+
+        assertThat(header.getName(), is(Headers.X_TRACE_ID));
+        assertThat(header.getValue(), is(traceId));
+    }
+
+    public static void shouldGenerateTraceIdIfGivenTraceIdIsNull() {
+        Header header = TraceId(null);
+
+        assertThat(header.getName(), is(Headers.X_TRACE_ID));
+        assertThat(header.getValue(), is(notNullValue()));
     }
 }
