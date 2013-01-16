dee8b1a104809be5f2f522fb4de1654ee1076379
Adam Esterline and Steve Neely <pair+ae+sn@rallydev.com>
2013-01-16 10:33:18 -0700

    Switched to finagle for backend

--- src/main/java/com/rallydev/client/http/RestClient.java
+++ src/main/java/com/rallydev/client/http/RestClient.java
@@ -1,39 +1,55 @@
 package com.rallydev.client.http;
 
-import com.rallydev.net.SharedResourcePool;
+import com.twitter.finagle.Service;
+import com.twitter.finagle.builder.ClientBuilder;
+import com.twitter.finagle.http.Http;
+import com.twitter.util.Duration;
+import com.twitter.util.Future;
+import com.twitter.util.Try;
+import org.apache.http.Header;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
+import org.jboss.netty.handler.codec.http.HttpMethod;
+import org.jboss.netty.handler.codec.http.HttpVersion;
 
 import java.io.IOException;
-import java.util.List;
-
-import static java.util.Arrays.asList;
+import java.nio.charset.Charset;
 
 public class RestClient {
-    private final RallyHttpClient httpClient;
-    private final SharedResourcePool<String> urlPool;
+    private Service<org.jboss.netty.handler.codec.http.HttpRequest, org.jboss.netty.handler.codec.http.HttpResponse> client;
 
-    public RestClient(String... urls) {
-        this(new RallyHttpClient(), asList(urls));
-    }
-
-    public RestClient(List<String> urls) {
-        this(new RallyHttpClient(), urls);
-    }
-
-    public RestClient(RallyHttpClient httpClient, String... urls) {
-        this(httpClient, asList(urls));
-    }
-
-    public RestClient(RallyHttpClient httpClient, List<String> urls) {
-        this.httpClient = httpClient;
-        this.urlPool = SharedResourcePool.of(urls);
+    public RestClient(String hosts) {
+        client = ClientBuilder.safeBuild(ClientBuilder.get()
+                .codec(Http.get())
+                .hosts(hosts)
+                .hostConnectionLimit(100));
     }
 
     public HttpResponse execute(HttpRequest request) {
-        return urlPool.use(new RequestTransaction(httpClient, request));
+        Future<org.jboss.netty.handler.codec.http.HttpResponse> responseFuture = client.apply(toRequest(request));
+        Try<org.jboss.netty.handler.codec.http.HttpResponse> responseTry = responseFuture.get(Duration.fromSeconds(2));
+        return toResponse(responseTry.apply());
+    }
+
+    public DefaultHttpRequest toRequest(HttpRequest request) {
+        DefaultHttpRequest nettyRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, getMethod(request), request.getURI());
+        nettyRequest.setContent(ChannelBuffers.copiedBuffer(request.getBody(), Charset.defaultCharset()));
+        for(Header header : request.getHeaders()) {
+            nettyRequest.addHeader(header.getName(), header.getValue());
+        }
+        return nettyRequest;
+    }
+
+    public HttpResponse toResponse(org.jboss.netty.handler.codec.http.HttpResponse response) {
+        return new HttpResponse(response.getStatus().getCode(), response.getContent().toString(Charset.defaultCharset()));
     }
 
     public void close() throws IOException {
-        httpClient.close();
+        client.release();
+    }
+
+    public HttpMethod getMethod(HttpRequest request) {
+        return HttpMethod.valueOf(request.getMethod().name());
     }
 
 }
